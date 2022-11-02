d56f9369d309c257b5f84708376aef4f2587fb8b
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2022-11-02 16:17:27 +0700

    Membuat method untuk menerima masukan

--- Denune Muhammad Kaisar K_20210140050_Exe2/Program.cs
+++ Denune Muhammad Kaisar K_20210140050_Exe2/Program.cs
@@ -12,6 +12,32 @@ namespace Denune_Muhammad_Kaisar_K_20210140050_Exe2
         private int[] den = new int[25];
         private int n;
 
+        public void read()
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Masukkan banyaknya element: ");
+                string s = Console.ReadLine();
+                //Mengubah string ke int
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 25))
+                    break;
+                else
+                    Console.WriteLine("\nMaksimal element adalah 25.\n");
+            }
+            Console.WriteLine("------------------");
+            Console.WriteLine(" Masukkan element ");
+            Console.WriteLine("------------------");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                den[i] = Int32.Parse(s1);
+            }
+        }
+       
 
         static void Main(string[] args)
         {
