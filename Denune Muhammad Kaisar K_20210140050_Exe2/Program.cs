149971362b121856234af12c137334be78b7d73b
TC1050 <denune.muhammad.ft21@mail.umy.ac.id>
2022-11-02 15:24:47 +0700

    Add project files.

---
+++ Denune Muhammad Kaisar K_20210140050_Exe2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denune_Muhammad_Kaisar_K_20210140050_Exe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
